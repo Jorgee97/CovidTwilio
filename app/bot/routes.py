628c690a211db849c9c4e2f0389fc34fc72bcb46
Jorge <ingjorgegomez@outlook.com>
2020-04-12 13:59:42 -0500

    feat: added option to menu and cleaned menu

--- app/bot/routes.py
+++ app/bot/routes.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request
-from twilio.twiml.messaging_response import MessagingResponse
 from .methods import *
 from ..data.scrapping import get_data, get_day_image
 
@@ -8,7 +7,7 @@ bot_bp = Blueprint('bot', __package__, url_prefix='/bot')
 options = {
     '0': information_numbers(),
     '1': information_graphic(),
-    '2': 'Option 2',
+    '2': information_total(),
     '3': information_filter(),
 }
 
@@ -16,10 +15,10 @@ options = {
 @bot_bp.route('', methods=['POST'])
 def bot_post():
     incoming_message = request.values.get('Body', '').lower()
-
+    menu = clear_string(incoming_message)
     if incoming_message in options:
         return select_menu_option(incoming_message, options)
-    if 'menu' in incoming_message:
+    if 'menu' in menu:
         return list_menu()
 
     return information_filter(incoming_message)

ca63b7e79621d79b00da52641346cd70ccb6861b
Jorge <ingjorgegomez@outlook.com>
2020-04-15 19:54:35 -0500

    feat: added option 3 to route because was causing a weir bug

--- app/bot/routes.py
+++ app/bot/routes.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, request
-from .methods import *
-from ..data.scrapping import get_data, get_day_image
+from .methods import (information_graphic, clear_string, information_filter, information_numbers, information_total,
+                      list_menu, select_menu_option, get_random_movie_or_series)
+from ..data.scrapping import get_data, get_day_image, get_movies_series
 
 bot_bp = Blueprint('bot', __package__, url_prefix='/bot')
 
 options = {
     '0': information_numbers(),
     '1': information_graphic(),
-    '2': information_total(),
-    '3': information_filter(),
+    '2': information_total()
 }
 
 
@@ -18,6 +18,8 @@ def bot_post():
     menu = clear_string(incoming_message)
     if incoming_message in options:
         return select_menu_option(incoming_message, options)
+    if incoming_message == '3':
+        return get_random_movie_or_series()
     if 'menu' in menu:
         return list_menu()
 
@@ -28,5 +30,11 @@ def bot_post():
 def bot_fetch_image_manually():
     get_data()
     get_day_image()
+    get_movies_series()
 
     return "Done."
+
+
+@bot_bp.route('/fetch/random', methods=['GET'])
+def bot_fetch_random_movie():
+    return get_random_movie_or_series()

00861adb3409d1cccf34dfee7470a87b5a654bd6
Jorge <ingjorgegomez@outlook.com>
2020-04-15 19:55:03 -0500

    feat: added get movies and series for the task to scrap

--- app/data/task.py
+++ app/data/task.py
@@ -1,7 +1,5 @@
 import time
-import datetime
-from .scrapping import get_data
-from .scrapping import get_day_image
+from .scrapping import get_data, get_movies_series, get_day_image
 
 SECONDS_PER_HOUR = 3600
 HOURS_PER_DAY = 24
@@ -9,4 +7,5 @@ HOURS_PER_DAY = 24
 while True:
     get_data()
     get_day_image()
+    get_movies_series()
     time.sleep(SECONDS_PER_HOUR*HOURS_PER_DAY)
